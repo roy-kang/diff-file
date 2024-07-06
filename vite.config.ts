1391eb6e2158c2bf6004a5b5d3871101bdbbd67b
Kang Dong <Dong.D.Kang@cn.ey.com>
2024-07-06 20:23:34 +0800

    build: 修正打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,16 @@ import monacoEditorPlugin from 'vite-plugin-monaco-editor'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     (monacoEditorPlugin as any).default({}) // 确保这里使用的是插件函数
   ],
+  build: {
+    outDir: './dist'
+  },
   server: {
+    host: '0.0.0.0',
     port: 8866
   }
 })
